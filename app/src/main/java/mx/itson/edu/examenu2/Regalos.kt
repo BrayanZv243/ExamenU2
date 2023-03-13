94cf63a94fcec19855aa2cf598ce8b82142a089e
Brayan Zavala <zavala_243@hotmail.com>
2023-03-13 12:56:47 -0700

    ps no se xD

--- app/src/main/java/mx/itson/edu/examenu2/Regalos.kt
+++ app/src/main/java/mx/itson/edu/examenu2/Regalos.kt
@@ -13,16 +13,40 @@ import android.widget.ImageView
 
 class Regalos : AppCompatActivity() {
     var adapter: RegalosAdapter? = null
-    var regalos = ArrayList<Detalles>()
+    var myRegalos = ArrayList<Detalles>()
+
+    var detalles: String = "250"
+    var globos: String = "300"
+    var peluches: String = "200"
+    var regalos: String = "150"
+    var tazas: String = "200"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regalos)
 
-        //cargarRegalos()
+        var menuOption: String? = intent.getStringExtra("menuType")
+        cargarRegalos(menuOption)
         val gridview: GridView = findViewById(R.id.myGrid)
-        adapter = RegalosAdapter(this,regalos)
+        adapter = RegalosAdapter(this,myRegalos)
         gridview.adapter = adapter
+    }
+
+    fun cargarRegalos(option:String?) {
+        when (option) {
+            "Detalles" -> {
+                myRegalos.add(Detalles(R.drawable.cumplebotanas,"Detalles",detalles))
+                myRegalos.add(Detalles(R.drawable.cumplecheve,"Detalles",detalles))
+                myRegalos.add(Detalles(R.drawable.cumpleescolar,"Detalles",detalles))
+                myRegalos.add(Detalles(R.drawable.cumplepaletas,"Detalles",detalles))
+                myRegalos.add(Detalles(R.drawable.cumplesnack,"Detalles",detalles))
+                myRegalos.add(Detalles(R.drawable.cumplevinos,"Detalles",detalles))
+            }
+
+            "Globos" -> {
+
+            }
+        }
     }
 
     class RegalosAdapter: BaseAdapter{
